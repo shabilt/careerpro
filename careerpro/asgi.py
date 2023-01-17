716a892538697be14928d62b25cd3bb052c79ab8
hskoori <hashidsharafkoori@gmail.com>
2023-01-18 02:01:03 +0530

    990

--- careerpro/asgi.py
+++ careerpro/asgi.py
@@ -1,10 +1,11 @@
+import os
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'careerpro.settings')
+
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter , URLRouter
 from chat import routing
 from main.middleware import TokenAuthMiddleware
-import os
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'careerpro.settings')
 
 application = ProtocolTypeRouter(
     {
