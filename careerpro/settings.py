f426550534c848cd7a028b61424acb5a0292986b
Hashid sharaf koori <hashidsharafkoori@gmail.com>
2023-01-29 17:01:46 +0530

    0066

--- careerpro/settings.py
+++ careerpro/settings.py
@@ -111,12 +111,25 @@ CORS_ALLOW_CREDENTIALS = True
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }  
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'v2_careerpro',
+        'USER': 'v2_careerprouser',
+        'PASSWORD': 'careeRprO00@2w2332:osperB',
+        'HOST': '139.59.60.16',
+        'PORT': '5432',
     }
-}  
+}
+
 
 AUTH_USER_MODEL = 'account.Account'
 
